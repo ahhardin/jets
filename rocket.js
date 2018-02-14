513dacff09888002d29e0e78efb5e19852129c57
Ashley Hardin <ashleyhardin@Ashleys-MacBook-Air.local>
2018-02-14 13:57:36 -0500

    improved Jorge's busted rocket

--- rocket.js
+++ rocket.js
@@ -1,6 +1,9 @@
 class Rocket {
-  constructor(name, color) {
+  constructor(name, color, size) {
     this.name = name;
     this.color = color;
+    this.size = size
   }
 }
+
+export default Rocket
